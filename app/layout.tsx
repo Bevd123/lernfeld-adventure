23b871b62b4f0c69fb1d336245684ab852c3c47c
Bevd123 <raphaellk1630@gmail.com>
2024-06-11 17:02:03 +0200

    added insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,14 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
 import { CookiesProvider } from 'next-client-cookies/server';
- 
+import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Analytics } from "@vercel/analytics/react"
+
 export default function RootLayout( { children, } : { children: React.ReactNode; } ) {
   return (
     <html lang="en">
+      <Analytics />
+      <SpeedInsights />
       <CookiesProvider>{children}</CookiesProvider>
       <body className={`${inter.className} antialiased`}></body>
     </html>
