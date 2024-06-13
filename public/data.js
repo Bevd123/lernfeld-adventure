06f2b4b36ffd1cdbd99b4018c207f77adf23b17d
Bevd123 <79664097+Bevd123@users.noreply.github.com>
2024-06-13 11:15:40 +0200

    removed assets

--- public/data.js
+++ public/data.js
@@ -3,47 +3,47 @@ export const sculptureList = [{
     time: "30.04 - 05.05",
     description: "Erste Ideenfindungsphase und erste Konzepte gefunden.",
     extra: "AB",
-    alt: "A bronze statue of two crossed hands delicately holding a human brain in their fingertips."
+    alt: ""
   }, {
     week: "2. Woche",
     time: "06.05 - 12.05",
     description: "Erste Konzepte und Prototypen entwickelt. ",
     extra: "",
-    alt: "A gigantic metallic flower sculpture with reflective mirror-like petals and strong stamens."
+    alt: ""
   }, {
     week: "3. Woche",
     time: "13.05 - 19.05",
     description: "Bisheriges Konzept verworfen.",
     extra: "",
-    alt: "The sculpture depicting a human head seems ever-present and solemn. It radiates calm and serenity."
+    alt: ""
   }, {
     week: "4. Woche",
     time: "20.05 - 26.05",
     description: "Neues Konzept entwickelt und ausgebaut.",
     extra: "",
-    alt: "Three monumental stone busts with the heads that are disproportionately large with somber faces."
+    alt: ""
   }, {
     week: "5. Woche",
     time: "27.05 - 02.06",
     description: "Erste Bedienung und Level entwickelt.",
     extra: "",
-    alt: "A tall sculpture made of three elements stacked on each other reminding of a human figure."
+    alt: ""
   }, {
     week: "6. Woche",
     time: "03.06 - 09.06",
     description: "/",
     extra: "",
-    alt: "An intricate wood sculpture of a warrior with a focused face on a horse adorned with patterns."
+    alt: ""
   }, {
     week: "7. Woche",
     time: "10.06 - 16.06",
     description: "/",
     extra: "",
-    alt: "The sculpture reminds a cascade of folds, quite different from bellies in classical sculptures."
+    alt: ""
   }, {
     week: "8. Woche",
     time: "17.06 - 23.06",
     description: "/",
     extra: "",
-    alt: "12 terracotta sculptures of solemn warriors, each with a unique facial expression and armor."
+    alt: ""
   }];
